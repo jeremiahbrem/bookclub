48b4fdf88cc5edfe590b3b5ff16c283b9b5a2d68
Jeremiah <jb@jbhometraining.com>
2020-09-05 22:08:14 -0500

    restored previous

--- node/server.js
+++ node/server.js
@@ -1,8 +1,9 @@
 /** Start server for bookclub. */
-
+const { DB_URI } = require("./config");
 const app = require("./app");
 const { PORT } = require("./config");
 
 app.listen(PORT, function() {
   console.log(`Server starting on port ${PORT}!`);
 });
+
