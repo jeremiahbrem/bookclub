25f1d221c2054a302aa8958a065d9a0a352e513f
Jeremiah <jb@jbhometraining.com>
2020-09-05 22:38:37 -0500

    production build

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 /** Start server for bookclub. */
+const express = require("express");
+const path = require("path");
 const { DB_URI } = require("./config");
 const app = require("./app");
 const { PORT } = require("./config");
 
 app.listen(PORT, function() {
   console.log(`Server starting on port ${PORT}!`);
+});
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
